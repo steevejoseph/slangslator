355af327ead5a3d119abf5aa3d37b980b6def4a7
Steeve Joseph <steevejoseph@knights.ucf.edu>
2024-11-23 15:41:54 -0500

    Update port

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/gorilla/mux"
 )
-
 
 func main() {
 	router := mux.NewRouter()
@@ -16,7 +17,14 @@ func main() {
 	router.HandleFunc("/quick-define", HandleQuickDefine)
 	router.HandleFunc("/quick-define/{term:.*}", HandleQuickDefine)
 
-	PORT := ":8080"
-	fmt.Printf("Server runnin on %s at %s\n", PORT, time.Now().Format("Monday, January 2, 2006 at 3:04 PM"))
-	http.ListenAndServe(PORT, router)
+	// Use the PORT environment variable, default to 8080 if not set
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Printf("Server running on %s at %s\n", port, time.Now().Format("Monday, January 2, 2006 at 3:04 PM"))
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("Server failed to start: ", err)
+	}
 }
